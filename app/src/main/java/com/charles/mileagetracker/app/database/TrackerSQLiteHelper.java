3a0ba51da92448368dd3ec8cfcec1b5e0dcde7e8
charles <charles.d.burton@gmail.com>
2014-06-02 21:19:22 -0600

    Updated database version

--- app/src/main/java/com/charles/mileagetracker/app/database/TrackerSQLiteHelper.java
+++ app/src/main/java/com/charles/mileagetracker/app/database/TrackerSQLiteHelper.java
@@ -10,7 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class TrackerSQLiteHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "trips.db";
-    private static final int DATABASE_VERSION = 12;
+    private static final int DATABASE_VERSION = 13;
 
     public TrackerSQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
